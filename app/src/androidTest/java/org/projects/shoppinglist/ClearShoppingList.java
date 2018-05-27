33fbb12915074d840e84d222156b85e433b8bf2d
Daniel Malmros Christiansen <daniel@bymalmros.dk>
2018-05-27 11:36:56 +0200

    Removed test attr from clearlist

--- app/src/androidTest/java/org/projects/shoppinglist/ClearShoppingList.java
+++ app/src/androidTest/java/org/projects/shoppinglist/ClearShoppingList.java
@@ -27,14 +27,10 @@ import static android.support.test.espresso.matcher.ViewMatchers.withText;
 import static org.hamcrest.Matchers.allOf;
 import static org.hamcrest.Matchers.is;
 
-//@LargeTest
-//@RunWith(AndroidJUnit4.class)
 public class ClearShoppingList {
 
-    //@Rule
     public ActivityTestRule<MainActivity> mActivityTestRule = new ActivityTestRule<>(MainActivity.class);
 
-    //@Test
     static void clearList() {
         // Added a sleep statement to match the app's execution delay.
         // The recommended way to handle such scenarios is to use Espresso idling resources:
